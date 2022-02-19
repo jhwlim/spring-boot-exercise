24c69de0723dd295815fb409e77ee80fa39097fc
jhwlim <jhwlim25@gmail.com>
2022-02-19 23:48:59 +0900

    feat : SecurityConfig 추가 및 기본 설정
    - rest api 적용에 따른
        - csrf과 formLogin을 disable 처리
        - 세션 생성 정책을 stateless 으로 설정
    - h2-console 경로를 security ignoring 처리

---
+++ security-rest/src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+
+                .formLogin().disable()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+}
