43f12fb8c0d432d01e50ec5ec9061cc48bdf3e10
jhwlim <jhwlim25@gmail.com>
2022-04-19 19:41:50 +0900

    feat : Quick Start 예제 추가

--- spring-cloud-stream-basic/src/main/java/com/example/Application.java
+++ spring-cloud-stream-basic/src/main/java/com/example/Application.java
@@ -2,12 +2,36 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Consumer<Person> log() {
+        return person -> {
+            System.out.println("Received : " + person);
+        };
+    }
+
+    public static class Person {
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public String toString() {
+            return this.name;
+        }
+
     }
 
 }
